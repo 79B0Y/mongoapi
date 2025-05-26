2d47b2142b2d688af61b74d5e10532eb572b5788
79B0Y <jackyao791104@gmail.com>
2025-05-26 18:34:07 +0800

    Update create_database.py

--- mongoapi_status_final_plus_db/mongoapi/handlers/create_database.py
+++ mongoapi_status_final_plus_db/mongoapi/handlers/create_database.py
@@ -1,22 +1,33 @@
-from flask import jsonify
-
 def handle(request, client):
-    data = request.get_json()
-    db_name = data["database"]
-    col_name = data.get("collection")
+    try:
+        data = request.get_json()
+        db_name = data['database']
+        collection_name = data.get('collection')
 
-    db = client[db_name]
-    created = False
+        db = client[db_name]
 
-    if col_name:
-        col = db[col_name]
-        col.insert_one({"init": True})
-        col.delete_many({"init": True})
-        created = True
+        if collection_name:
+            col = db[collection_name]
+            col.insert_one({"init": True})  # 触发创建数据库和集合
+            collections = db.list_collection_names()
+            return {
+                "created": True,
+                "database": db_name,
+                "collections": collections
+            }
+        else:
+            # 创建一个临时集合再删除它，确保数据库创建
+            tmp_col = db["__init__"]
+            tmp_col.insert_one({"_": 1})
+            db.drop_collection("__init__")
+            return {
+                "created": True,
+                "database": db_name,
+                "collections": db.list_collection_names()
+            }
 
-    collections = db.list_collection_names()
-    return {
-        "created": created or bool(collections),
-        "database": db_name,
-        "collections": collections
-    }
+    except Exception as e:
+        return {
+            "error": str(e),
+            "created": False
+        }
